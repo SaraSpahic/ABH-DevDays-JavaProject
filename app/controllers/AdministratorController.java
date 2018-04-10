49b400a4d2282a9d2f379de20537927b2a7c0b1a
Sara Spahic <sara_spahic@yahoo.com>
2018-04-10 22:05:55 +0100

    SS-3: Added fileUpload function on backend, added gallery tab on frontend

--- app/controllers/AdministratorController.java
+++ app/controllers/AdministratorController.java
@@ -1,8 +1,11 @@
 package controllers;
 
+import play.mvc.Http;
 import services.AdministratorService;
 
+import play.mvc.Result;
 import javax.inject.Inject;
+import java.io.File;
 
 /**
  * The type Administrator controller.
@@ -20,4 +23,28 @@ public class AdministratorController extends BaseController {
     public void setService(final AdministratorService service) {
         this.service = service;
     }
+
+    public Result fileUpload() {
+        Http.MultipartFormData<File> body = request().body().asMultipartFormData();
+        Http.MultipartFormData.FilePart<File> picture = body.getFile("file");
+        if (picture != null) {
+            String fileName = picture.getFilename();
+            String contentType = picture.getContentType();
+            File file = picture.getFile();
+            return ok("File uploaded");
+        } else {
+            flash("error", "Missing file");
+            return badRequest();
+        }
+    }
+
+    public Result checkPreFlight() {
+        response().setHeader("Access-Control-Allow-Origin", "*");       // Need to add the correct domain in here!!
+        response().setHeader("Access-Control-Allow-Methods", "POST");   // Only allow POST
+        response().setHeader("Access-Control-Max-Age", "300");          // Cache response for 5 minutes
+        response().setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");         // Ensure this header is also allowed!
+        return ok();
+    }
+
+
 }
