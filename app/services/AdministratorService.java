f7527ecbe9a669d45ad53a01d8c5a2aaa756162e
SaraSpahic <sara_spahic@yahoo.com>
2018-04-10 13:01:20 +0200

    SS-5: Make basic statistics for admin panel (#3)
    * SS-5: Make basic statistics for admin panel
    
    * Code reformatting.

--- app/services/AdministratorService.java
+++ app/services/AdministratorService.java
@@ -1,5 +1,12 @@
 package services;
 
+import models.helpers.AdministratorStatistics;
+import models.tables.City;
+import models.tables.Cuisine;
+import models.tables.Restaurant;
+import models.tables.User;
+import org.hibernate.criterion.Projections;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -11,6 +18,19 @@ public class AdministratorService extends BaseService {
 
     @Inject
     private AdministratorService() {
+    }
+
+    public AdministratorStatistics getStatistics() {
+        return new AdministratorStatistics(
+                ((Number) getSession().createCriteria(Restaurant.class)
+                        .setProjection(Projections.rowCount()).uniqueResult()).intValue(),
+                ((Number) getSession().createCriteria(Cuisine.class)
+                        .setProjection(Projections.rowCount()).uniqueResult()).intValue(),
+                ((Number) getSession().createCriteria(City.class)
+                        .setProjection(Projections.rowCount()).uniqueResult()).intValue(),
+                ((Number) getSession().createCriteria(User.class)
+                        .setProjection(Projections.rowCount()).uniqueResult()).intValue()
+        );
     }
 
 }
